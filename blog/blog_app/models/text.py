bc8d2a0ca3db689234f273ad08782bf0b948a43d
Gizem Çırıkka <gizemtuncer17@gmail.com>
2022-08-11 14:41:57 +0300

    text  model created

---
+++ blog/blog_app/models/text.py
@@ -0,0 +1,26 @@
+from curses.ascii import US
+from decimal import Clamped
+from distutils.command.upload import upload
+from enum import unique
+from statistics import mode
+from tabnanny import verbose
+from django.db import models
+from autoslug import AutoSlugField
+from blog_app.models import CategoryModel
+from django.contrib.auth.models import User
+
+class TextModel(models.Model):
+    title = models.CharField(max_length=50)
+    image = models.ImageField(upload_to= 'text_image')
+    context = models.TextField()
+    created_at= models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    slug = AutoSlugField(populate_from= 'title', unique=True)
+    categories = models.ManyToManyField(CategoryModel, related_name='text')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='text_author')
+    
+    class Meta:
+        verbose_name = 'Text'
+        verbose_name_plural = 'Texts'
+        db_name = 'Text'    
+        
