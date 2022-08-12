35a4e237efdab46241fe98fc5f0b8c9ac233b411
Gizem Çırıkka <gizemtuncer17@gmail.com>
2022-08-12 13:51:08 +0300

    abstract model created

--- blog/blog_app/models/comment.py
+++ blog/blog_app/models/comment.py
@@ -4,15 +4,13 @@ from venv import create
 from django.db import models
 from django.contrib.auth.models import User
 from blog_app.models import TextModel
+from blog_app.abstract_models import DateAbstractModel
 
 
 class CommentModel(models.Model):
     writer = models.ForeignKey('account.CustomUserModel', on_delete=models.CASCADE, related_name='comment')
     text = models.ForeignKey(TextModel, on_delete=models.CASCADE, related_name ='comments')
     comment = models.TextField()
-    created_at= models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    
     class Meta:
         db_table = 'comment'
         verbose_name = 'Comment'
--- blog/blog_app/models/text.py
+++ blog/blog_app/models/text.py
@@ -5,24 +5,24 @@ from tabnanny import verbose
 from django.db import models
 from autoslug import AutoSlugField
 from blog_app.models import CategoryModel
+from blog_app.abstract_models import DateAbstractModel
 
-#ck editor import edilecek!
-class TextModel(models.Model):
+# ck editor import edilecek!
+
+
+class TextModel(DateAbstractModel):
     title = models.CharField(max_length=50)
-    image = models.ImageField(upload_to= 'text_image')
+    image = models.ImageField(upload_to='text_image')
     context = models.TextField()
-    created_at= models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    slug = AutoSlugField(populate_from= 'title', unique=True)
+    slug = AutoSlugField(populate_from='title', unique=True)
     categories = models.ManyToManyField(CategoryModel, related_name='text')
-    author = models.ForeignKey('account.CustomUserModel', on_delete=models.CASCADE, related_name='text_author')
-    
+    author = models.ForeignKey(
+        'account.CustomUserModel', on_delete=models.CASCADE, related_name='text_author')
+
     class Meta:
         verbose_name = 'Text'
         verbose_name_plural = 'Texts'
-        db_table = 'Text'  
+        db_table = 'Text'
 
     def __str__(self):
         return self.title
-        
-        
