8e061fd9190efd6927bce169c6ad9fd2f8a099d7
Gizem Çırıkka <gizemtuncer17@gmail.com>
2022-08-15 16:37:02 +0300

    paginator created!

--- blog/blog_app/view/home.py
+++ blog/blog_app/view/home.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from blog_app.models import TextModel
+from django.core.paginator import Paginator
 
 def home(request):
-    return render (request,'pages/index.html',context={})
+    text = TextModel.objects.order_by('-id')
+    page = request.GET.get('page')
+    paginator = Paginator(text,2)
+    
+    return render (request,'pages/index.html',context={
+        'texts' : paginator.get_page(page)
+        })
