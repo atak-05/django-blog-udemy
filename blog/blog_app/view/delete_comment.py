5c9c3f20f90017f96d257cbae0a2cf57f16fbc90
Gizem Çırıkka <gizemtuncer17@gmail.com>
2022-08-17 16:50:04 +0300

    message framework  added!!!

--- blog/blog_app/view/delete_comment.py
+++ blog/blog_app/view/delete_comment.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from blog_app.models import CommentModel
 from django.shortcuts import get_object_or_404,redirect
+# Message Framework in Django
+from django.contrib import messages
 
 
 
@@ -9,6 +11,8 @@ def delete_comment (request, id):
     comment = get_object_or_404(CommentModel, id=id)
     if comment.writer == request.user or comment.text.author == request.user:
         comment.delete()
+        # Message Framework in Django
+        messages.success(request, 'Your comment has been successfully deleted')
         return redirect('detail', slug=comment.text.slug)
     
     return redirect('home')
