e4d63cb6c05b87bf52b7a2d4df223bee3aee5365
Gizem Çırıkka <gizemtuncer17@gmail.com>
2022-08-17 15:11:40 +0300

    delete  post created!

--- blog/blog_app/urls.py
+++ blog/blog_app/urls.py
@@ -2,7 +2,7 @@ import imp
 from unicodedata import name
 from django.urls import path
 from blog_app.view import contact
-from blog_app.view import home, category, my_text, detail,add_text,update_text
+from blog_app.view import home, category, my_text, detail,add_text,update_text,delete_text
 
 urlpatterns = [
     path('', home, name='home'),
@@ -12,4 +12,5 @@ urlpatterns = [
     path('detail/<slug:slug>', detail, name='detail'),
     path('add-text/', add_text, name='add_text'),
     path('update-text/<slug:slug>', update_text ,name='update_text'),
+    path('delete-text/<slug:slug>', delete_text ,name='delete_text'),
 ]
