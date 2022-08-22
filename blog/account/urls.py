e5380c170c123c7cdbe2766bd8fefa50e60e970c
Gizem Çırıkka <gizemtuncer17@gmail.com>
2022-08-22 15:18:07 +0300

    Profile Detail View - view of django editede!!

--- blog/account/urls.py
+++ blog/account/urls.py
@@ -2,7 +2,7 @@ from re import template
 from unicodedata import name
 from django.urls import path
 from account.forms import sign_in_user
-from account.views import exit, changePassword,changeProfile, sign_In_User
+from account.views import exit, changePassword,changeProfile, sign_In_User ,ProfileDetailView
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [ 
@@ -13,4 +13,5 @@ urlpatterns = [
     path('change-password',changePassword, name='change_password' ),
     path('change-profile',changeProfile, name='change_profile' ),
     path('sign-in_user',sign_In_User, name='sign_in_user' ),
+    path('user/<str:username>', ProfileDetailView.as_view(), name='profil')
  ]
--- blog/account/views/__init__.py
+++ blog/account/views/__init__.py
@@ -2,3 +2,4 @@ from .exit import exit
 from .change_password import changePassword
 from .changed_profile import changeProfile
 from .sign_in_user import sign_In_User
+from .profile import ProfileDetailView
