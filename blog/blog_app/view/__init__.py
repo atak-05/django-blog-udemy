08e195e25a8618d656edc99c4b92a8b06ab5150b
Gizem Çırıkka <gizemtuncer17@gmail.com>
2022-08-22 14:20:46 +0300

    CreateView - view of django editede!!

--- blog/blog_app/view/__init__.py
+++ blog/blog_app/view/__init__.py
@@ -3,7 +3,7 @@ from .home import home
 from .category import CategoryListView
 from .my_text import my_text
 from .detail import DetailView
-from .add_text import add_text
+from .add_text import AddTextCreateView
 from .update_text import update_text
 from .delete_text import DeleteTextDeleteView
 from .delete_comment import delete_comment
--- blog/blog_app/view/add_text.py
+++ blog/blog_app/view/add_text.py
@@ -1,15 +1,38 @@
+from urllib import request
 from django.shortcuts import render, redirect
 from blog_app.forms import AddTextForm
 from django.contrib.auth.decorators import login_required
+from django.views.generic import CreateView
+from blog_app.models import TextModel
+from django.urls import reverse
 
-
-@login_required(login_url='/')
-def add_text(request):
-    form = AddTextForm(request.POST or None, files = request.FILES or None)
-    if form.is_valid():
+class AddTextCreateView(CreateView):
+    template_name = 'pages/add_text.html'
+    model = TextModel
+    fields = ('title' , 'image' , 'context', 'categories')
+    
+    def get_success_url(self):
+        return reverse('detail', kwargs= {
+            'slug' : self.object.slug
+        })
+    
+    def form_valid(self, form):
         text=form.save(commit=False)
-        text.author = request.user
+        text.author = self.request.user
         text.save()
         form.save_m2m()
-        return redirect('detail', slug=text.slug)
-    return render(request, 'pages/add_text.html', context={'form': form})
+        return super().form_valid(form)
+
+
+
+
+# @login_required(login_url='/')
+# def add_text(request):
+#     form = AddTextForm(request.POST or None, files = request.FILES or None)
+#     if form.is_valid():
+#         text=form.save(commit=False)
+#         text.author = request.user
+#         text.save()
+#         form.save_m2m()
+#         return redirect('detail', slug=text.slug)
+#     return render(request, 'pages/add_text.html', context={'form': form})
